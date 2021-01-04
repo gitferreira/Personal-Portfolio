758618927dbcc92ba79692bdb02d90ac1646dd2e
gitferreira <iniferre96@gmail.com>
2021-01-04 18:32:51 +0100

    Form

--- app.js
+++ app.js
@@ -123,6 +123,54 @@ function scrollShow3(){
 }
 
 
+// Form
+
+window.addEventListener("DOMContentLoaded", function () {
+
+  
+	const form = document.getElementById("my-form");
+
+	const status = document.getElementById("status");
+  
+
+  
+	function success() {
+	  form.reset();
+	  status.classList.add("success");
+	  status.innerHTML = "Submited, thanks!";
+	}
+  
+	function error() {
+	  status.classList.add("error");
+	  status.innerHTML = "Oops! Something went wrong";
+	}
+  
+	// handle the form submission event
+  
+	form.addEventListener("submit", function (ev) {
+	  ev.preventDefault();
+	  const data = new FormData(form);
+	  ajax(form.method, form.action, data, success, error);
+	});
+  });
+  
+  // helper function for sending an AJAX request
+  
+  function ajax(method, url, data, success, error) {
+	const xhr = new XMLHttpRequest();
+	xhr.open(method, url);
+	xhr.setRequestHeader("Accept", "application/json");
+	xhr.onreadystatechange = function () {
+	  if (xhr.readyState !== XMLHttpRequest.DONE) return;
+	  if (xhr.status === 200) {
+		success(xhr.response, xhr.responseType);
+	  } else {
+		error(xhr.status, xhr.response, xhr.responseType);
+	  }
+	};
+	xhr.send(data);
+  }
+  
 
 
 
